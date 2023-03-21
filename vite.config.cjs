0be9cde27ae3f24ece79b79787f0b065e289954e
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-03-21 22:31:44 +0800

    first commit

---
+++ vite.config.cjs
@@ -0,0 +1,17 @@
+// import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    //   rollupOptions: {
+    //     input: {
+    //       main: resolve(__dirname, "src/index.html"),
+    //       nested: resolve(__dirname, "src/nested/nested.html"),
+    //     },
+    //   },
+  },
+  plugins: [react()],
+});
