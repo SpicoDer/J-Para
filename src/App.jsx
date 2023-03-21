3bbb62e244f243f0d9191954764f06522023d83e
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-03-21 22:38:14 +0800

    Create routes and pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import Profile from './pages/Profile.jsx';
+import ForgotPassword from './pages/ForgotPassword.jsx';
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
