534cb278ff25f11d40d702339381f68a9eab6d7e
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-03-23 13:00:25 +0800

    Change and show the active page in log navigation.

--- src/components/Log/Login/LoginNav.jsx
+++ src/components/Log/Login/LoginNav.jsx
@@ -1,14 +1,32 @@
+import { useLocation, useNavigate } from 'react-router';
+
 function LoginNav() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const activeLink = path =>
+    location.pathname === path && 'border-b-2 border-prim-400';
+
   return (
     <div className='flex items-center gap-4 border-b-2 border-gray-200'>
-      <span className='cursor-pointer border-b-2 border-prim-400 px-4 pb-2 uppercase text-txt-light'>
+      <span
+        className={`cursor-pointer px-4 pb-2 uppercase text-txt-light ${activeLink(
+          '/sign-in'
+        )}`}
+        onClick={() => navigate('/sign-in')}
+      >
         sign in
       </span>
-      <span className='cursor-pointer px-4 pb-2 uppercase text-txt-light'>
+      <span
+        className={`cursor-pointer px-4 pb-2 uppercase text-txt-light ${activeLink(
+          '/sign-up'
+        )}`}
+        onClick={() => navigate('/sign-up')}
+      >
         sign up
       </span>
     </div>
   );
 }
-
+//
 export default LoginNav;
