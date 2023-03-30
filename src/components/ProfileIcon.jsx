1d567195fdf08cb2a89951b8d7eee3bb5e22716c
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-03-30 16:03:24 +0800

    Change ui of ProfileIcon

--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -1,7 +1,7 @@
 function ProfileIcon({ size, textSize }) {
   return (
     <span
-      className={`grid h-${size} w-${size} place-items-center rounded-full ${textSize} bg-prim-400 text-white`}
+      className={`grid h-${size} w-${size} place-items-center rounded-full ${textSize} cursor-pointer bg-prim-400 text-white`}
     >
       JC
     </span>
