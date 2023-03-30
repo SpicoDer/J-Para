75bf3211a79096f96a08fd2dc639e7d8a802410f
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-03-30 11:28:01 +0800

    Create modal.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
 import ModalSetting from '../components/Home/Settings/ModalSetting';
+import { useState } from 'react';
 
 function Home() {
+  const [toggleOpen, setToggleOpen] = useState(false);
+
   return (
     <div>
-      <ModalSetting />
+      <div
+        onClick={() => {
+          setToggleOpen(!toggleOpen);
+        }}
+        className='grid h-12 w-12 place-items-center rounded-full bg-prim-400'
+      >
+        <p>JC</p>
+      </div>
+      <ModalSetting toggle={setToggleOpen} state={toggleOpen} />
     </div>
   );
 }
