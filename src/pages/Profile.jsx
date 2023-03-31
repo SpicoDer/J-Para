966a1fed74ac4344fd36dcb51b0d980bb33a76c7
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-03-31 10:46:03 +0800

    Make profile page responsive.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,17 @@
+import Map from '../components/Home/Map/Map.jsx';
 import ProfileBody from '../components/Profile/ProfileBody/ProfileBody.jsx';
 import ProfileHeading from '../components/Profile/ProfileHeading';
 
 function Profile() {
   return (
-    <div>
-      <ProfileHeading />
-      <ProfileBody />
+    <div className='h-screen w-screen lg:flex'>
+      <div className='lg:w-[600px]'>
+        <ProfileHeading />
+        <ProfileBody />
+      </div>
+      <div className='hidden h-full w-full lg:block'>
+        <Map />
+      </div>
     </div>
   );
 }
