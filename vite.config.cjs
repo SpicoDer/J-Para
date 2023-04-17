a1ea4fead77059878072e0915b8c61ed1643199e
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-04-17 09:30:25 +0800

    Change root dir.

--- vite.config.cjs
+++ vite.config.cjs
@@ -3,15 +3,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    //   rollupOptions: {
-    //     input: {
-    //       main: resolve(__dirname, "src/index.html"),
-    //       nested: resolve(__dirname, "src/nested/nested.html"),
-    //     },
-    //   },
-  },
+  // root: 'src',
+  // build: {
+  //   outDir: '../dist',
+  //   //   rollupOptions: {
+  //   //     input: {
+  //   //       main: resolve(__dirname, "src/index.html"),
+  //   //       nested: resolve(__dirname, "src/nested/nested.html"),
+  //   //     },
+  //   //   },
+  // },
   plugins: [react()],
 });
